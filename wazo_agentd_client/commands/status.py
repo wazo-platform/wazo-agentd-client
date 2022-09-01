ffdd8bcaedf48e7e26a20f45c78c0996dc36e0e6
nissou91 <achohra@wazo.io>
2022-09-01 14:30:33 -0400

    Use agents _ResponseProcessor for status exceptions handling

--- wazo_agentd_client/commands/status.py
+++ wazo_agentd_client/commands/status.py
@@ -2,18 +2,20 @@
 # Copyright 2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from wazo_agentd_client.command import AgentdCommand
 
+from wazo_lib_rest_client.command import RESTCommand
+from .agents import _ResponseProcessor
 
-class StatusCommand(AgentdCommand):
+class StatusCommand(RESTCommand):
     resource = 'status'
 
     def __call__(self):
+        _resp_processor = _ResponseProcessor()
         headers = self._get_headers()
         url = self.base_url
         r = self.session.get(url, headers=headers)
 
         if r.status_code != 200:
-            self.raise_from_response(r)
+            _resp_processor.generic(r)
 
         return r.json()
