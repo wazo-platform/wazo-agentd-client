cfbf7fc29e1dfa48c330f171070a74be68dc1b7f
Julien Alie <jalie@wazo.io>
2025-12-11 23:20:06 -0500

    add user agent subscribe/unsubscribe to/from queue

--- wazo_agentd_client/commands/agents.py
+++ wazo_agentd_client/commands/agents.py
@@ -121,6 +121,18 @@ class AgentsCommand(RESTCommand):
         req = self._req_factory.status_all(tenant_uuid=tenant_uuid, recurse=recurse)
         return self._execute(req, self._resp_processor.status_all)
 
+    def subscribe_user_agent_to_queue(self, queue_id, tenant_uuid=None):
+        tenant_uuid = tenant_uuid or self._client.tenant_uuid
+        user_req_factory = _RequestFactory(self._client.url())
+        req = user_req_factory.subscribe_user_agent_to_queue(queue_id, tenant_uuid=tenant_uuid)
+        return self._execute(req, self._resp_processor.generic)
+
+    def unsubscribe_user_agent_from_queue(self, queue_id, tenant_uuid=None):
+        tenant_uuid = tenant_uuid or self._client.tenant_uuid
+        user_req_factory = _RequestFactory(self._client.url())
+        req = user_req_factory.unsubscribe_user_agent_from_queue(queue_id, tenant_uuid=tenant_uuid)
+        return self._execute(req, self._resp_processor.generic)
+
     def _execute(self, req, processor_fun, timeout=None):
         timeout = timeout if timeout is not None else self.timeout
         resp = self.session.send(self.session.prepare_request(req), timeout=timeout)
@@ -313,6 +325,20 @@ class _RequestFactory:
             url, additional_headers=additional_headers, params=params
         )
 
+    def subscribe_user_agent_to_queue(self, queue_id, tenant_uuid=None):
+        url = f'{self._base_url}/users/me/agents/queues/{queue_id}/subscribe'
+        additional_headers = {}
+        if tenant_uuid:
+            additional_headers['Wazo-Tenant'] = tenant_uuid
+        return self._new_put_request(url, additional_headers=additional_headers)
+
+    def unsubscribe_user_agent_from_queue(self, queue_id, tenant_uuid=None):
+        url = f'{self._base_url}/users/me/agents/queues/{queue_id}/unsubscribe'
+        additional_headers = {}
+        if tenant_uuid:
+            additional_headers['Wazo-Tenant'] = tenant_uuid
+        return self._new_put_request(url, additional_headers=additional_headers)
+
     def _new_get_request(self, url, additional_headers=None, params=None):
         headers = dict(self._headers)
         if additional_headers:
@@ -329,3 +355,14 @@ class _RequestFactory:
             data = json.dumps(obj)
             headers['Content-Type'] = 'application/json'
         return requests.Request('POST', url, headers, data=data, params=params)
+
+    def _new_put_request(self, url, obj=None, additional_headers=None, params=None):
+        headers = dict(self._headers)
+        if additional_headers:
+            headers.update(additional_headers)
+        if obj is None:
+            data = None
+        else:
+            data = json.dumps(obj)
+            headers['Content-Type'] = 'application/json'
+        return requests.Request('PUT', url, headers, data=data, params=params)
